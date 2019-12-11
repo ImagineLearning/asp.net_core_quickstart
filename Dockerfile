aaec1417863c4919a7b405cb99e96bb1484e707c
Alex.Quintero <alex.quintero@imaginelearning.com>
2019-12-11 09:04:48 -0700

    update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM microsoft/dotnet:2.1.301-sdk-alpine AS builder
+FROM mcr.microsoft.com/dotnet/core/sdk:3.1-alpine AS builder
 WORKDIR /app
  
 # copy csproj and restore as distinct layers
@@ -10,7 +10,7 @@ COPY ./quickstart/. ./
 RUN dotnet publish -c Release -o out
  
 # build runtime image
-FROM microsoft/dotnet:2.1.1-aspnetcore-runtime-alpine
+FROM mcr.microsoft.com/dotnet/core/aspnet:3.1-alpine
 WORKDIR /app
 COPY --from=builder /app/out .
 ENTRYPOINT ["dotnet", "quickstart.dll"]
--- quickstart/Startup.cs
+++ quickstart/Startup.cs
@@ -10,13 +10,13 @@ namespace Quickstart
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc( opts => opts.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.EnvironmentName == "Dev")
             {
                 app.UseDeveloperExceptionPage();
             }
